8eaeecea5f1914e083aa1daee8e85d10b798f481
Xavier M <golf4Xavier@gmail.com>
2022-06-16 11:59:55 -0400

    adding classes for models

---
+++ Basketball MVC/Basketball MVC/Models/Coach.cs
@@ -0,0 +1,24 @@
+﻿namespace Basketball_MVC.Models
+{
+    public class Coach
+    {
+        public string Name;
+        public Team Team;
+        public List<Team> PastTeam;
+        public DateTime StartYear;
+        public int Wins;
+        public int Losses;
+        public string FavoriteFood;
+
+        public Coach(string name, Team team, List<Team> pastTeam, DateTime startYear, int wins, int losses, string favoriteFood)
+        {
+            Name = name;
+            Team = team;
+            PastTeam = pastTeam;
+            StartYear = startYear;
+            Wins = wins;
+            Losses = losses;
+            FavoriteFood = favoriteFood;
+        }
+    }
+}
