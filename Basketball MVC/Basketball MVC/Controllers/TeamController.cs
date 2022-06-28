91501dba74150e34c4f0785dc36f673f5c9fef7e
Xavier M <golf4Xavier@gmail.com>
2022-06-28 11:22:11 -0400

    adjusting team controller

--- Basketball MVC/Basketball MVC/Controllers/TeamController.cs
+++ Basketball MVC/Basketball MVC/Controllers/TeamController.cs
@@ -22,6 +22,18 @@ namespace Basketball_MVC.Controllers
         }
         public IActionResult Create()
         {
+            List<Coach> coaches = new List<Coach>();
+            foreach(Coach c in db.Coaches.ToList())
+            {
+                Team temp = db.Teams.FirstOrDefault(t => t.CoachId == c.Id);
+                if(temp != null)
+                {
+                    coaches.Add(c);
+                }
+            }
+            //one liner
+            //List<Coach> Coaches = db.Coaches.Where(c => db.Teams.FirstOrDefault(t => t.CoachId == c.Id)== null).ToList();
+
             ViewBag.Coaches = new SelectList(db.Coaches.ToList(), "Id", "Name");
             return View(new Team());
         }
